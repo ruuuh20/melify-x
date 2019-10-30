489c1259f4ee8bafe58bdc65e4ff62e06be41397
ruuuh20 <pkim2320@gmail.com>
2019-10-29 23:16:21 -0400

    redirect

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
-// Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "8db64f013fcb45f792b396ccdcfdde1d";
-export const redirectUri = "http://localhost:3000/redirect";
+// export const redirectUri = "http://localhost:3000/redirect";
+export const redirectUri = "http://localhost:8888/callback/";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
